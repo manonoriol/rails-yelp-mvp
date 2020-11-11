b608b28a112e484017e2c8056d5c3c83afe6ab39
Manon Oriol <oriol.manon@gmail.com>
2020-11-11 16:50:38 +0100

    completed rails yelp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  CATEG = %w[chinese italian japanese french belgian]
+
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEG }
+end
---
+++ db/migrate/20201111113527_change_phone_number_to_be_a_string_in_restaurant.rb
@@ -0,0 +1,5 @@
+class ChangePhoneNumberToBeAStringInRestaurant < ActiveRecord::Migration[6.0]
+  def change
+    change_column :restaurants, :phone_number, :string
+  end
+end
